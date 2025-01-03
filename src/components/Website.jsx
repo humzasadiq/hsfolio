6642f483c6eec79657e7113bac7c7bea4f38ab8d
humzasadiq <humzasadiq52@gmail.com>
2025-01-04 04:41:40 +0500

    optimization

--- src/components/Website.jsx
+++ src/components/Website.jsx
@@ -7,26 +7,30 @@ const Website = ({ url, opened, setOpened }) => {
       className={`fixed w-full h-full inset-0 bg-black/50 z-[59] transition-opacity duration-300 ${
         opened ? "opacity-100 pointer-events-auto" : "opacity-0 pointer-events-none"
       }`}
-      onClick={() => setOpened(false)}
+      onClick={() => setOpened(false)} // Close modal on click outside
     >
-    <div
-    className="text-[30px] text-black dark:text-white cursor-pointer absolute top-4 left-6 bg-white dark:bg-zinc-800 rounded-lg z-1000"
-    onClick={() => setOpened(false)}
-    >
-    <IoMdArrowBack />
-    </div>
+      {/* Back Button */}
+      <div
+        className="text-[30px] text-black dark:text-white cursor-pointer absolute top-4 left-6 bg-white dark:bg-zinc-800 rounded-lg z-1000"
+        onClick={() => setOpened(false)}
+      >
+        <IoMdArrowBack />
+      </div>
 
-      <div className="flex justify-center items-center h-full">
+      {/* Modal Content */}
+      <div className="flex justify-center items-center h-full p-2">
         <div className="bg-white dark:bg-zinc-800 p-2 rounded-lg shadow-lg w-full h-[90vh]">
-          <iframe
-            src={url}
-            width="100%"
-            height="100%"
-            sandbox="allow-scripts allow-forms"
-            loading="lazy"
-            title="Custom Title"
-            className="rounded-lg"
-          />
+          {opened && ( // Conditionally render the iframe
+            <iframe
+              src={url}
+              width="100%"
+              height="100%"
+              sandbox="allow-scripts allow-forms"
+              loading="lazy"
+              title="Custom Title"
+              className="rounded-lg"
+            />
+          )}
         </div>
       </div>
     </div>
