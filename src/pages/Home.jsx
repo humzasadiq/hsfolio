b531f0557f6b1be650a0fbf644660a1797cbb902
humzasadiq <humzasadiq52@gmail.com>
2025-01-05 01:13:32 +0500

    skill-final

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -14,13 +14,13 @@ function Home() {
         // <div className="bg-zinc-100 dark:bg-zinc-900">
       <div className="xl:w-[1200px] md:mx-auto h-full border-x border-zinc-100 dark:border-zinc-800 bg-white dark:bg-zinc-950">
         <Hero/>
+        <Projects/>
+        <Website/>
+        <EducationAndExperience/>
         <div className="mb-10">
           <Skills type="Skills" sub="tools"/>
           <Skills type="Programming Languages" sub="languages"/>
         </div>
-        <Projects/>
-        <Website/>
-        <EducationAndExperience/>
         <hr className="mt-12 border border-zinc-300 dark:border-zinc-800" />
         <Footer theme={theme} />
       </div>
