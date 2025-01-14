86e6a9510434388ff5ceafafcad0dca4b073f10a
humzasadiq <humzasadiq52@gmail.com>
2025-01-15 02:44:48 +0500

    .

--- src/components/sections/Contacts.jsx
+++ src/components/sections/Contacts.jsx
@@ -3,14 +3,14 @@ const Contacts = () => {
     return (
         <div className="flex flex-row items-start justify-start gap-4 mt-5 ml-6 md:ml-6 lg:ml-[95px] mb-10 ">
             <a
-            class="inline-flex text-base font-semibold text-[#1E1E1E] dark:text-[#F1F1F1]  hover:decoration-solid underline decoration-dotted underline-offset-4 transition-all duration-300" 
+            class="inline-flex text-sm sm:text-base font-semibold text-[#1E1E1E] dark:text-[#F1F1F1]  hover:decoration-solid underline decoration-dotted underline-offset-4 transition-all duration-300" 
             href="https://github.com/humzasadiq"
             target="_blank"
             >
             <FaGithub className='w-5 h-5 mr-1.5'/>Github
             </a>
             <a
-            class="text-base font-semibold text-[#1E1E1E] dark:text-[#F1F1F1]  hover:decoration-solid underline decoration-dotted underline-offset-4 transition-all duration-300" 
+            class="text-sm sm:text-base font-semibold text-[#1E1E1E] dark:text-[#F1F1F1]  hover:decoration-solid underline decoration-dotted underline-offset-4 transition-all duration-300"  
             href="mail:humzasadiq92@gmail.com"
             target="_blank"
             >
