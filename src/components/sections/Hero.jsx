07713df403afc6ad9367cae978408737b0a4109e
humzasadiq <humzasadiq52@gmail.com>
2025-01-24 03:56:48 +0500

    .

--- src/components/sections/Hero.jsx
+++ src/components/sections/Hero.jsx
@@ -3,7 +3,7 @@ import favicon from '/favicon.png';
 import Contacts from './Contacts';
 export default function Hero() {
     return (
-        <section id="hero" className="pt-20 sm:pt-20 md:pt-20 flex px-6 lg:px-16 mb-10">
+        <section id="hero" className="pt-20 sm:pt-20 md:pt-20 flex px-6 lg:px-16 mb-10 lg:mb-0 items-center justify-center">
             
             <div className="self-center rounded-lg border border-zinc-300 dark:border-zinc-800 bg-[#eae8e5] dark:bg-zinc-900 p-6">
                 <div className="hs-tooltip [--placement:right] hs-tooltip-toggle w-20">
@@ -18,7 +18,7 @@ export default function Hero() {
                 </div>
                 <div className="flex gap-2 align-center flex-wrap md:flex-nowrap">
                 <div className="lg:w-[80%] text-zinc-900 dark:text-zinc-100 self-center">
-                    <h2 className="text-xl">{user_info.main.role} <p className=' inline text-base font-light'>from</p><span><img src="/flag-pakistan.svg" alt="Pakistan" className="inline mx-1 mb-1 w-8 h-8"/></span></h2>
+                    <h2 className="text-xl">{user_info.main.role} <p className=' inline text-base font-light'>from</p><span><img src="/flag-pakistan.svg" alt="Pakistan" className="inline mx-1 mb-1 w-6 h-6"/></span></h2>
                     <h1 className="heading-myfont font-black mt-3 text-3xl md:text-4xl lg:text-5xl w-full lg:w-[85%]">
                         {user_info.main.name}
                     </h1>
